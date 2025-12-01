c4c92cc0c76fa2595c3c1429b232c91d122f7ef5
MertHasanAslan <merthasanaslan325@gmail.com>
2025-12-01 11:48:02 +0300

    Initial commit, created the structure.

---
+++ src/main/java/com/banking/banking_app_java/BankingAppJavaApplication.java
@@ -0,0 +1,13 @@
+package com.banking.banking_app_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingAppJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingAppJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/banking/banking_app_java/BankingAppJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.banking_app_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingAppJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
